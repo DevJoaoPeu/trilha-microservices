7e5e7119712bf8cbc85bc3b71386c0c36ff948e5
DevJoaoPeu <joaocanaldroid@gmail.com>
2025-04-18 19:34:28 -0300

    feat: create module to database postgres

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,17 +1,10 @@
 import { Module } from '@nestjs/common';
 import { UsersModule } from './users/users.module';
 import { TypeOrmModule } from '@nestjs/typeorm';
+import { DatabaseModule } from './database/database.module';
 
 @Module({
-  imports: [
-    TypeOrmModule.forRoot({
-      type: 'sqlite',
-      database: 'db',
-      entities: [__dirname + '/**/*.entity.{ts,js}'],
-      synchronize: true,
-    }),
-    UsersModule,
-  ],
+  imports: [UsersModule, DatabaseModule],
   controllers: [],
   providers: [],
 })
---
+++ src/database/database.module.ts
@@ -0,0 +1,19 @@
+import { Module } from '@nestjs/common';
+import { TypeOrmModule } from '@nestjs/typeorm';
+import { UserEntity } from 'src/users/entities/user.entity';
+
+@Module({
+  imports: [
+    TypeOrmModule.forRoot({
+      type: 'postgres',
+      database: 'messengers',
+      username: 'postgres',
+      port: 5432,
+      host: 'localhost',
+      password: 'password',
+      entities: [UserEntity],
+      synchronize: true,
+    }),
+  ],
+})
+export class DatabaseModule {}
